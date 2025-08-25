f51cd95fca67d32dbd002fd1803d7685a7ef317b
Ming <2847919699@qq.com>
2025-08-25 15:59:04 +0800

    Redis自增策略： 1.每天一个key，方便统计订单量 2.ID构造是 时间戳+计数器

--- src/test/java/com/hmdp/HmDianPingApplicationTests.java
+++ src/test/java/com/hmdp/HmDianPingApplicationTests.java
@@ -5,6 +5,7 @@ import com.hmdp.entity.Shop;
 import com.hmdp.service.impl.ShopServiceImpl;
 import com.hmdp.utils.CacheClient;
 import com.hmdp.utils.RedisData;
+import com.hmdp.utils.RedisIdWorker;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.redis.core.StringRedisTemplate;
@@ -12,7 +13,7 @@ import org.springframework.data.redis.core.StringRedisTemplate;
 import javax.annotation.Resource;
 
 import java.time.LocalDateTime;
-import java.util.concurrent.TimeUnit;
+import java.util.concurrent.*;
 
 import static com.hmdp.utils.RedisConstants.CACHE_SHOP_KEY;
 
@@ -27,6 +28,11 @@ class HmDianPingApplicationTests {
 
     @Resource
     private StringRedisTemplate stringRedisTemplate;
+
+    @Resource
+    private RedisIdWorker redisIdWorker;
+
+
 /*    @Test
     void testSaveShop() throws InterruptedException {
         Shop shop = shopService.getById(1L);
@@ -34,12 +40,8 @@ class HmDianPingApplicationTests {
         cacheClient.setWithLogicalExpire(CACHE_SHOP_KEY + 1L, shop, 10L, TimeUnit.SECONDS);
     }*/
 
-
     @Test
     void testSaveShop() throws InterruptedException {
-
-
-
 
         Shop shop = shopService.getById(1L);
         cacheClient.setWithLogicalExpire(CACHE_SHOP_KEY + 1L, shop, 10L, TimeUnit.SECONDS);
@@ -47,6 +49,32 @@ class HmDianPingApplicationTests {
         // 从Redis读取验证
         String json = stringRedisTemplate.opsForValue().get(CACHE_SHOP_KEY + 1L);
         System.out.println("Redis中存储的数据: " + json);
+    }
+
+
+    private ExecutorService es = Executors.newFixedThreadPool(500);
+
+
+    @Test
+    void testIdWorker() throws InterruptedException {
+        CountDownLatch latch = new CountDownLatch(300);
+
+
+        Runnable task = () -> {
+            for (int i = 0; i < 1000; i++) {
+                long id = redisIdWorker.nextId("order");
+                System.out.println("id: " + id);
+            }
+            latch.countDown();
+        };
+
+        long begin = System.currentTimeMillis();
+        for (int i = 0; i < 300; i++) {
+            es.submit(task);
+        }
+        latch.await();
+        long end = System.currentTimeMillis();
+        System.out.println("耗时: " + (end - begin));
     }
 
 }
