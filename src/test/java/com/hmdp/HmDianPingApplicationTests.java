6168385c2dc1c509dc9e8214799b9ce2cc006477
Ming <2847919699@qq.com>
2025-09-07 00:55:11 +0800

    HyperLogLog 误差小，占用内存小 uv统计 独立访客量

--- src/test/java/com/hmdp/HmDianPingApplicationTests.java
+++ src/test/java/com/hmdp/HmDianPingApplicationTests.java
@@ -110,4 +110,37 @@ class HmDianPingApplicationTests {
             stringRedisTemplate.opsForGeo().add(key,locations);
         }
     }
+
+    @Test
+    void testHyperLogLog() {
+        String[] values = new String[1000];
+        int j = 0;
+        for (int i = 0; i < 1000000; i++) {
+            j = i % 1000;
+            values[j] = "user_"+ i;
+            if (j == 999){
+                //发送到redis
+                stringRedisTemplate.opsForHyperLogLog().add("hl2",values);
+            }
+        }
+        //统计数量
+        Long count = stringRedisTemplate.opsForHyperLogLog().size("hl2");
+        System.out.println("count = " + count);
+
+    }
+
+    @Test
+    void testHyperLogLog2() {
+        String[] values = new String[1000];
+        for (int i = 0; i < 1000; i++) {
+            values[i] = "user_" + i;
+        }
+        // 发送到redis
+        stringRedisTemplate.opsForHyperLogLog().add("hl2", values);
+
+        // 统计数量
+        Long count = stringRedisTemplate.opsForHyperLogLog().size("hl2");
+        System.out.println("count = " + count);
+    }
+
 }
