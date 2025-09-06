419e3bd94a08c305b6fc521abf7e7082281e4488
Ming <2847919699@qq.com>
2025-09-06 14:44:53 +0800

    发送帖子推送到粉丝收件箱 拉模式/读扩散 推模式/写扩散 用户量少、没有大v 滚动分页 sorted set 使用时间戳

--- src/main/java/com/hmdp/config/RedissonConfig.java
+++ src/main/java/com/hmdp/config/RedissonConfig.java
@@ -13,8 +13,14 @@ public class RedissonConfig {
     public RedissonClient redissonClient(){
         // 配置
         Config config = new Config();
-        config.useSingleServer().setAddress("redis://127.0.0.1:6379");
-        // 创建RedissonClient对象
+//        config.useSingleServer().setAddress("redis://127.0.0.1:6379");
+//        // 创建RedissonClient对象
+//        return Redisson.create(config);
+
+        // 确认这里使用的是配置文件中的地址，而不是硬编码的 127.0.0.1
+        config.useSingleServer()
+                .setAddress("redis://120.79.222.247:6379")  // 应该使用配置的地址
+                .setPassword("English629");  // 添加密码
         return Redisson.create(config);
     }
 }
